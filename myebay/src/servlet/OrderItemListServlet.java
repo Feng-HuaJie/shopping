c6b8ba1aae0d1d61704313c8323c4df8e9b94abf
Feng-HuaJie <40983532+Feng-HuaJie@users.noreply.github.com>
2018-07-24 22:40:50 +0800

    Update OrderItemListServlet.java

--- myebay/src/servlet/OrderItemListServlet.java
+++ myebay/src/servlet/OrderItemListServlet.java
@@ -13,6 +13,6 @@ public class OrderItemListServlet extends HttpServlet
  
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("listOrderItem.jsp").forward(request, response);
+        request.getRequestDispatcher("listOrderItem").forward(request, response);
     }
 }
