720b33e26bf8b95758acc9aec40e5d600a1bd61c
Feng-HuaJie <40983532+Feng-HuaJie@users.noreply.github.com>
2018-07-24 12:49:12 +0800

    Update ProductDao.java

--- myebay/src/dao/ProductDao.java
+++ myebay/src/dao/ProductDao.java
@@ -10,48 +10,65 @@ import java.util.List;
 
 import domain.Product;
  
-public class ProductDao {
-	
-	Product pd = null;
-	Connection c = null;
-	PreparedStatement ps = null;
-	List<Product> pds = null;
-	ResultSet rs = null;
  
-    public Product getProductById(int id) {
-        String sql = "select * from product where id = ?";
-
+public class ProductDao {
+ 
+ 
+    public Product getProduct(int id) {
+        Product result = null;
+ 
         try {
-            c = DBUtil.getConn();
-        	ps = c.prepareStatement(sql);
+            Class.forName("com.mysql.jdbc.Driver");
+ 
+            Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cart?characterEncoding=UTF-8",
+                    "root", "admin");
+ 
+            String sql = "select * from product where id = ?";
+ 
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, id);
+ 
             ResultSet rs = ps.executeQuery();
  
             if (rs.next()) {
-                pd = new Product();
-                pd.setId(id);
+                result = new Product();
+                result.setId(id);
+ 
                 String name = rs.getString(2);
                 float price = rs.getFloat(3);
-                pd.setName(name);
-                pd.setPrice(price);
+ 
+                result.setName(name);
+                result.setPrice(price);
  
             }
  
+            ps.close();
+ 
+            c.close();
+ 
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }finally {
-        	DBUtil.close(rs, ps, c);
         }
-        return pd;
+        return result;
     }
  
     public List<Product> ListProduct() {
-        pds = new ArrayList<Product>();
-        String sql = "select * from product order by id desc";
-        
+        List<Product> products = new ArrayList<Product>();
+ 
         try {
-        	c = DBUtil.getConn();
-        	ps = c.prepareStatement(sql);
+            Class.forName("com.mysql.jdbc.Driver");
+ 
+            Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cart?characterEncoding=UTF-8",
+                    "root", "admin");
+ 
+            String sql = "select * from product order by id desc";
+ 
+            PreparedStatement ps = c.prepareStatement(sql);
+ 
             ResultSet rs = ps.executeQuery();
  
             while (rs.next()) {
@@ -60,18 +77,24 @@ public class ProductDao {
                 String name = rs.getString(2);
                 float price = rs.getFloat(3);
  
-                pd.setId(id);
-                pd.setName(name);
-                pd.setPrice(price);
-                pds.add(product);
+                product.setId(id);
+                product.setName(name);
+                product.setPrice(price);
+                products.add(product);
  
             }
+ 
+            ps.close();
+ 
+            c.close();
+ 
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }finally {
-        	DBUtil.close(rs, ps, c);
         }
-        return pds;
+        return products;
     }
 }
